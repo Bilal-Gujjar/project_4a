3c22d545dd2c32605a3c9a26ccffb615c150e356
Bilal Gujjar <bilaltahir515@gmail.com>
2020-12-21 10:29:40 +0500

    Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+//import { useEffect, useRef, useState } from 'react';
+import useWebAnimations from '@wellyshen/use-web-animations';
 
 function App() {
+  const {ref,playState,getAnimation} = useWebAnimations({
+    keyframes:[
+      {transform: "translate(0px)",background:"pink"},
+      {transform: "translate(500px)",background:"yellow"}
+    ],
+    timing:{
+      duration: 3000,
+      iterations:4,
+      direction:"alternate",
+      easing:"ease-in-out",
+    },
+    onUpdate:({playState,animate,animation}) => {
+      console.log("Play State",playState);
+      console.log("animate",animate);
+      console.log("animation",animation);
+    }
+    
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <div>Animation State {playState}</div>
+      <div ref={ref} style= {{background:'red',width:"100px",height:"250px"}}>
+      Hello World
+      </div>
+      <button onClick={()=>{
+        getAnimation().play();
+      }}>Play</button>
+      
+      <button onClick={()=>{
+        getAnimation().pause();
+      }}>Pause</button>
+      
+     </div>
   );
 }
 
 export default App;
+
+
+
+/* const inputE1 =useRef(null);
+  const [val,setVal] = useState("");
+  useEffect(()=>{
+    inputE1.current.focus();
+
+  },[]); */
